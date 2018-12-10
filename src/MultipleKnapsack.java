34a365bee46f73b960b2d4c8da0b6d6b33672b8c
Nicholas jones <jonesnich8@ecs.vuw.ac.nz>
2018-12-10 20:33:31 +1300

    Adding Project to Github

---
+++ src/MultipleKnapsack.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class MultipleKnapsack {
+
+	private int[] values = {2, 3, 1, 4, 5};
+	private int[] weights = {1, 2, 3, 3, 5};
+	private int size = values.length;
+	private int maxWeight = 7;
+	private int table[];
+	
+	private MultipleKnapsack() {
+
+		System.out.print("Values: \t");
+		for(int x = 0; x < values.length; x++) {
+			System.out.print(values[x] + " ");
+		}
+		System.out.println();
+		System.out.print("Weights: \t");
+		for(int x = 0; x < weights.length; x++) {
+			System.out.print(weights[x] + " ");
+		}
+		System.out.println();
+		System.out.println("Max Weight: \t" + maxWeight);
+		
+		fillTable();
+	}
+
+	/**
+	 * for i from 0 to Capacity
+	 * 		for j from 0 to Amount of items - 1
+	 * 			if weight of item j <= i
+	 * 				table i = max of table i or (table i - weight) + value
+	 * 
+	 */
+	
+	private void fillTable() {
+		table = new int[maxWeight+1];
+		
+		for(int i = 0; i <= maxWeight; i++) {
+			for(int j = 0; j < size; j++) {
+				
+				int weight = weights[j];
+				int value = values[j];
+				
+				if(weight <= i) {
+					table[i] = Math.max(table[i], table[i-weight] + value);
+				}
+				
+			}
+		}
+		System.out.println("Highest Value that can be achieved: " + table[maxWeight]);
+		System.out.println("Complexity: O(size*maxWeight)");
+	}
+	
+	public static void main(String[] arguments) {
+		new MultipleKnapsack();
+	}
+}
